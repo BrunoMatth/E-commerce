dbd77e2bbbeb041ff2f047b40466edc558a45b67
Bruno Oliveira <brunoworkn@gmail.com>
2022-09-24 22:48:36 -0300

    feat: fakeApi

---
+++ e-commerce/src/service/index.js
@@ -0,0 +1,32 @@
+export const fakeApi = [
+    {
+        name:"Teclado mecânico da Red Dragon", 
+        category:"Periférico",
+        price: 1999.99
+    },
+    {
+        name:"Teclado mecânico da Red Dragon", 
+        category:"Periférico",
+        price: 1999.99
+    },
+    {
+        name:"Teclado mecânico da Red Dragon", 
+        category:"Periférico",
+        price: 1999.99
+    },
+    {
+        name:"Teclado mecânico da Red Dragon", 
+        category:"Periférico",
+        price: 1999.99
+    },
+    {
+        name:"Teclado mecânico da Red Dragon", 
+        category:"Periférico",
+        price: 1999.99
+    },
+    {
+        name:"Teclado mecânico da Red Dragon", 
+        category:"Periférico",
+        price: 1999.99
+    },
+]
